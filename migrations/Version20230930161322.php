ce07c31938e6054e2b5ba1a4a1190c49a353035e
Ринат <warmobigames@gmail.com>
2023-09-30 20:21:34 +0400

    init commit

---
+++ migrations/Version20230930161322.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230930161322 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE offer ADD description TEXT NOT NULL');
+        $this->addSql('ALTER TABLE offer ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE offer ADD date_created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+        $this->addSql('ALTER TABLE offer ADD image VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE offer ADD image_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('ALTER TABLE offer ADD short_description VARCHAR(1024) DEFAULT NULL');
+        $this->addSql('ALTER TABLE offer RENAME COLUMN name TO title');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE offer ADD name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE offer DROP title');
+        $this->addSql('ALTER TABLE offer DROP description');
+        $this->addSql('ALTER TABLE offer DROP slug');
+        $this->addSql('ALTER TABLE offer DROP date_created');
+        $this->addSql('ALTER TABLE offer DROP image');
+        $this->addSql('ALTER TABLE offer DROP image_updated_at');
+        $this->addSql('ALTER TABLE offer DROP short_description');
+    }
+}
---
+++ src/Entity/Offer.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\OfferRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation\UploadableField;
+
+#[ORM\Entity(repositoryClass: OfferRepository::class)]
+#[ApiResource]
+class Offer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(type:"string", length:255)]
+    private string $title;
+
+    #[ORM\Column(type:"text", length:255)]
+    private string $description;
+
+    #[ORM\Column(type:"string", length:255)]
+    #[Gedmo\Slug(fields: ['title'])]
+    private string $slug;
+
+    #[ORM\Column(type:"datetime")]
+    private \DateTime $date_created;
+
+    #[ORM\Column(type:"string", length:255, nullable:true)]
+    private string $image;
+
+    #[ORM\Column(type:"datetime", nullable:true)]
+    private \DateTime $imageUpdatedAt;
+
+
+    #[ORM\Column(type:"string", length:1024, nullable:true)]
+    private ?string $shortDescription;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     */
+    public function setSlug(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateCreated(): \DateTime
+    {
+        return $this->date_created;
+    }
+
+    /**
+     * @param \DateTime $date_created
+     */
+    public function setDateCreated(\DateTime $date_created): void
+    {
+        $this->date_created = $date_created;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageFile(): string
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param string $imageFile
+     */
+    public function setImageFile(string $imageFile): void
+    {
+        $this->imageFile = $imageFile;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getImageUpdatedAt(): \DateTime
+    {
+        return $this->imageUpdatedAt;
+    }
+
+    /**
+     * @param \DateTime $imageUpdatedAt
+     */
+    public function setImageUpdatedAt(\DateTime $imageUpdatedAt): void
+    {
+        $this->imageUpdatedAt = $imageUpdatedAt;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getShortDescription(): ?string
+    {
+        return $this->shortDescription;
+    }
+
+    /**
+     * @param string|null $shortDescription
+     */
+    public function setShortDescription(?string $shortDescription): void
+    {
+        $this->shortDescription = $shortDescription;
+    }
+}
